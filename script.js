885b0fe4e8bc6d0450c6cbb2bcdf1ab877610619
August A <153690815+CodelessA@users.noreply.github.com>
2024-05-29 00:24:12 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
 //----Navigation settings Start----//
-document.querySelectorAll('#navigation-ul li a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+document.querySelectorAll("#navigation-ul li a").forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        const targetId = this.getAttribute('href').substring(1);
-        const targetSection = document.getElementById(targetId);
-        
-        document.querySelector('.section-change').scrollTo({
-            left: targetSection.offsetLeft,
-            behavior: 'smooth'
-        });
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    document.querySelector(".section-change").scrollTo({
+      left: targetSection.offsetLeft,
+      behavior: "smooth",
     });
+  });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const navItems = document.querySelectorAll("#navigation-ul a");
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      // Delete 'active' class
+      navItems.forEach((nav) => nav.classList.remove("active"));
+
+      // Add 'active' class
+      this.classList.add("active");
+    });
+  });
+});
+
 //----Navigation settings End----//
 
 //---Info Panel controller start----//
-  document.addEventListener('DOMContentLoaded', function () {
-    const movies = document.querySelectorAll('#movies-div div');
-    const games = document.querySelectorAll('#games-div div');
+document.addEventListener("DOMContentLoaded", function () {
+  const movies = document.querySelectorAll("#movies-div div");
+  const games = document.querySelectorAll("#games-div div");
 
-    const infoContent = document.getElementById('info-content');
-    const infoContentG = document.getElementById('info-contentG');
+  const infoContent = document.getElementById("info-content");
+  const infoContentG = document.getElementById("info-contentG");
 
-    movies.forEach(movie => {
-      movie.addEventListener('click', function () {
-        const movieInfo = this.querySelector('p').innerText;
-        infoContent.innerText = `Informacje o wybranym elemencie: ${movieInfo}`;
-      });
+  movies.forEach((movie) => {
+    movie.addEventListener("click", function () {
+      const movieInfo = this.querySelector("p").innerText;
+      infoContent.innerText = `Informacje o wybranym elemencie: ${movieInfo}`;
     });
-    
-    games.forEach(game => {
-      game.addEventListener('click', function () {
-        const gameInfo = this.querySelector('p').innerText;
-        infoContentG.innerText = `Informacje: ${gameInfo}`;
-      });
+  });
+
+  games.forEach((game) => {
+    game.addEventListener("click", function () {
+      const gameInfo = this.querySelector("p").innerText;
+      infoContentG.innerText = `Informacje: ${gameInfo}`;
     });
+  });
 });
 //---Info Panel controller end----//
